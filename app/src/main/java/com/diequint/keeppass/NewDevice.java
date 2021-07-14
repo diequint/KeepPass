3b75aab823ec1fba8dc1eebc384c22d066a90546
Diego Quintana <diequint99@gmail.com>
2021-07-14 01:27:40 -0500

    Creation of New Device activity

---
+++ app/src/main/java/com/diequint/keeppass/NewDevice.java
@@ -0,0 +1,14 @@
+package com.diequint.keeppass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewDevice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_device);
+    }
+}
