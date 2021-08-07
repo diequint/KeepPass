6a35bc25de4a8eef3f68563f91365bdfe501ee4e
Diego Quintana <diequint99@gmail.com>
2021-08-06 23:22:46 -0500

    Integration of QR codes to sync data

--- app/src/main/java/com/diequint/keeppass/NewDevice.java
+++ app/src/main/java/com/diequint/keeppass/NewDevice.java
@@ -1,9 +1,11 @@
 package com.diequint.keeppass;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,12 +13,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class NewDevice extends AppCompatActivity {
     ImageView step1, step2, step3;
     TextView confirmId, errPassword, syncKeys, phoneReady;
     EditText passwordBox;
-    Button got_it_2, got_it_3, endNewDev;
-    String savedPass="tamal123";
+    Button got_it_1, got_it_2, got_it_3, endNewDev;
+    String qrData="";
+    Boolean band=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,7 @@ public class NewDevice extends AppCompatActivity {
         step1 = findViewById(R.id.step1);
         step2 = findViewById(R.id.step2);
         step3 = findViewById(R.id.step3);
+        got_it_1 = findViewById(R.id.got_it_1);
         confirmId = findViewById(R.id.confirmId);
         passwordBox = findViewById(R.id.passwordBox);
         errPassword = findViewById(R.id.errPassword);
@@ -41,35 +48,75 @@ public class NewDevice extends AppCompatActivity {
         int opacity = 100; // from 0 to 255
         switch (view.getId()) {
             case R.id.got_it_1:
-                confirmId.setVisibility(View.VISIBLE);
-                passwordBox.setVisibility(View.VISIBLE);
-                /*if (password doesn't match) {
-                    errPassword.setVisibility(View.VISIBLE);
-                } the line below goes in if condition as well*/
-                got_it_2.setVisibility(View.VISIBLE);
-                step2.setVisibility(View.VISIBLE);
-                step1.setAlpha((float) 0.5); //Sets opacity by half
+                if (!qrData.equals("")) {//Since this executes in background I leave it in an if
+                    confirmId.setVisibility(View.VISIBLE);
+                    passwordBox.setVisibility(View.VISIBLE);
+                    got_it_2.setVisibility(View.VISIBLE);
+                    step2.setVisibility(View.VISIBLE);
+                    step1.setAlpha((float) 0.5);
+                } else { //This means anything has been read yet
+                    new IntentIntegrator(this)
+                            .setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+                            .setTorchEnabled(false)
+                            .setBeepEnabled(true)
+                            .setPrompt((String) getText(R.string.autoSnap))
+                            .initiateScan();
+                }
                 break;
             case R.id.got_it_2:
-                if (passwordBox.getText().toString().equals(savedPass)) {
+                if (passwordBox.getText().toString().equals(qrData)) {
                     errPassword.setVisibility(View.INVISIBLE);
                     syncKeys.setVisibility(View.VISIBLE);
                     got_it_3.setVisibility(View.VISIBLE);
                     step3.setVisibility(View.VISIBLE);
                     step2.setAlpha((float) 0.5);
+                    qrData = "";
+                    band=true;
                 } else {
                     errPassword.setVisibility(View.VISIBLE);
                     errPassword.setText(R.string.passNoMatch);
                 }
                 break;
             case R.id.got_it_3:
-                phoneReady.setVisibility(View.VISIBLE);
-                endNewDev.setVisibility(View.VISIBLE);
+                if (!qrData.equals("")) {
+                    phoneReady.setVisibility(View.VISIBLE);
+                    endNewDev.setVisibility(View.VISIBLE);
+                } else {
+                    new IntentIntegrator(this)
+                            .setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+                            .setTorchEnabled(false)
+                            .setBeepEnabled(true)
+                            .setPrompt((String) getText(R.string.autoSnap))
+                            .initiateScan();
+                }
                 break;
             case R.id.endNewDev:
                 Intent intent = new Intent(this, MyKeys.class);
                 startActivity(intent);
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result!=null) {
+            if (result.getContents() != null) {
+                qrData = result.getContents();
+                autoClickButton();
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.cancelled, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void autoClickButton() {
+        if (band){
+            got_it_3.performClick();
+        }else {
+            got_it_1.performClick();
         }
     }
 }
---
+++ app/src/main/java/com/diequint/keeppass/SyncData.java
@@ -0,0 +1,92 @@
+package com.diequint.keeppass;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class SyncData extends AppCompatActivity {
+    Button genCode, readCode, addCode;
+    ImageView generatedQR;
+    String qrData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sync_data);
+        getSupportActionBar().hide();
+
+        genCode = findViewById(R.id.genCode);
+        readCode = findViewById(R.id.readCode);
+        addCode = findViewById(R.id.addCode);
+        generatedQR = findViewById(R.id.generatedQR);
+
+        genCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MultiFormatWriter formatWriter = new MultiFormatWriter();
+                try {
+                    BitMatrix bitMatrix = formatWriter.encode("sopita", BarcodeFormat.QR_CODE,500,500);
+                    BarcodeEncoder myEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = myEncoder.createBitmap(bitMatrix);
+                    generatedQR.setScaleX(1.5f);
+                    generatedQR.setScaleY(1.5f);
+                    generatedQR.setImageBitmap(bitmap);
+                } catch (Exception e){
+                    Toast.makeText(getApplicationContext(), getString(R.string.cancelled), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        addCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MultiFormatWriter formatWriter = new MultiFormatWriter();
+                try {
+                    BitMatrix bitMatrix = formatWriter.encode("sopita", BarcodeFormat.QR_CODE,500,500);
+                    BarcodeEncoder myEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = myEncoder.createBitmap(bitMatrix);
+                    generatedQR.setImageBitmap(bitmap);
+                } catch (Exception e){
+                    Toast.makeText(getApplicationContext(), getString(R.string.cancelled), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        new IntentIntegrator(this)
+                .setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+                .setTorchEnabled(false)
+                .setBeepEnabled(true)
+                .setPrompt((String) getText(R.string.autoSnap))
+                .initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result!=null) {
+            if (result.getContents() != null) {
+                qrData = result.getContents();
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.cancelled, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
